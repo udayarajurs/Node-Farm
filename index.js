6ab8941d0c6a8e61fb5109c16c1771122edbed2d
udaya raj urs G <Udayarajurs7@gmail.com>
2022-07-15 18:13:48 +0530

    working on fs

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const hello = 'Hello world';
-console.log(hello);
+const fs = require("fs");
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written successfully!");
+
+// Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.error("ERROR! 😢😢");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("File written successfully😉😉");
+      });
+    });
+  });
+});
+
+console.log("will read file!");
